53916fa797ad9dd3bb05405c65f33f7528499473
Alejandro Lay <alejandro.lay30@gmail.com>
2025-03-30 22:46:53 +0200

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('olistictimer').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/app.js',
+                '/manifest.json',
+                '/assets/icon-192.png',
+                '/assets/icon-512.png',
+                '/assets/logo-header.png'
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
