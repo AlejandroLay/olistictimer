1fcee494ebb8e104f484c841c35791206d939db7
Alejandro Lay <alejandro.lay30@gmail.com>
2025-03-30 22:59:37 +0200

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,1 +1,27 @@
-// serviceWorker.js content will be added manually
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open('olistictimer').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './style.css',
+                './app.js',
+                './manifest.json',
+                './assets/icon-192.png',
+                './assets/icon-512.png',
+                './assets/logo-header.png',
+                './assets/splash-1024.png'
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
